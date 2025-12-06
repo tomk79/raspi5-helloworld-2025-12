e1d86d58b026f9d6d7b27d6169be6974478b7ab4
Tomoya Koyanagi <tomk79@gmail.com>
2025-12-06 23:25:29 +0900

    debug

--- node/start.js
+++ node/start.js
@@ -1,32 +1,8 @@
 const gpiod = require('node-gpiod');
 
-// Raspberry Pi 5では gpiochip4 を使用
-const chipPath = '/dev/gpiochip4';
-const GPIO_PIN = 17;
+// デバッグ: node-gpiodのエクスポート内容を確認
+console.log('node-gpiod exports:', Object.keys(gpiod));
+console.log('node-gpiod:', gpiod);
 
-// GPIOチップを開く
-const chip = gpiod.openChip(chipPath);
-console.log(`GPIOチップを開きました: ${chipPath}`);
-
-// GPIO17を出力として設定
-const line = chip.getLine(GPIO_PIN);
-line.requestOutputMode('LED', 0);
-console.log(`GPIO${GPIO_PIN}を出力モードで設定しました`);
-
-let value = 0;
-
-const interval = setInterval(() => {
-  value = value ^ 1; // 0/1反転
-  line.setValue(value);
-  console.log(`LED ${value ? 'ON' : 'OFF'}`);
-}, 500); // 0.5秒間隔
-
-// プログラム終了時の処理
-process.on('SIGINT', () => {
-  clearInterval(interval);
-  line.setValue(0);  // LEDを消灯
-  line.release();     // GPIOリソースを解放
-  chip.close();
-  console.log('\nプログラムを終了します');
-  process.exit();
-});
+// まずはAPIを確認するためのテストコード
+// 実際のLED制御は後で実装
