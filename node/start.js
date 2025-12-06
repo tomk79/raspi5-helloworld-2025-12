a708e7359b5112de23cf9d1e06173f6900680669
Tomoya Koyanagi <tomk79@gmail.com>
2025-12-06 23:01:01 +0900

    onoff -> node-libgpiod

--- node/start.js
+++ node/start.js
@@ -1,13 +1,22 @@
-const { Gpio } = require('onoff');
-const led = new Gpio(17, 'out');
+const { Chip } = require('node-libgpiod');
+
+// Raspberry Pi 5では /dev/gpiochip4 を使用
+const chip = new Chip(4);
+const led = chip.getLine(17);
+
+// 出力モードで設定
+led.requestOutputMode();
+
 let value = 0;
 
 setInterval(() => {
   value = value ^ 1; // 0/1反転
-  led.writeSync(value);
+  led.setValue(value);
 }, 500); // 0.5秒間隔
 
 process.on('SIGINT', () => {
-  led.writeSync(0);
-  led.unexport();
+  led.setValue(0);
+  led.release();
+  chip.close();
+  process.exit();
 });
